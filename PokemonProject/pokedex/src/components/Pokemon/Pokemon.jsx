b87d536bc7bc27c28658b29178d7f21136e1c202
satyaprakashpatel024 <satyaprakashpatel61@gmail.com>
2024-03-12 20:36:01 +0530

    changes saved

--- PokemonProject/pokedex/src/components/Pokemon/Pokemon.jsx
+++ PokemonProject/pokedex/src/components/Pokemon/Pokemon.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import './Pokemon.css';
 
 const Pokemon = ({ name, image }) => {
 	return (
-		<div>
-			<div>{name}</div>
-			<div>
-				<img src={image} alt="image" />
+		<div className="pokemon">
+			<div className="pokemon-name">{name}</div>
+			<div className="">
+				<img className="pokemon-image" src={image} alt="image" />
 			</div>
 		</div>
 	);
--- PokemonProject/pokedex/src/components/PokemonList/PokemonList.jsx
+++ PokemonProject/pokedex/src/components/PokemonList/PokemonList.jsx
@@ -7,21 +7,21 @@ const PokemonList = () => {
 	const [pokemonList, setPokemonList] = useState([]);
 	const [isloading, setIsLoading] = useState(true);
 
-    const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon";
+	const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon";
 
 	async function downloadPokemon() {
-        // this downloads the first 20 pokemons
+		// this downloads the first 20 pokemons
 		const response = await axios.get(POKEDEX_URL);
-        // this gives the array of pokemons and url from result
+		// this gives the array of pokemons and url from result
 		const pokemonResults = response.data.results;
-		console.log(pokemonResults,'ppppppppppp');
-        // iterating over the array of pokemons and fetching the data from urls 
-        // then creating the array of promises that will download the details of these 20 pokemons
+		// console.log(pokemonResults, "ppppppppppp");
+		// iterating over the array of pokemons and fetching the data from urls
+		// then creating the array of promises that will download the details of these 20 pokemons
 		const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));
-        // this will wait for all the promises to be resolved
-        // this will create an array of pokemon objects
+		// this will wait for all the promises to be resolved
+		// this will create an array of pokemon objects
 		const pokemonData = await axios.all(pokemonResultPromise);
-        // now iterate over each pokemon objects from array and extract name,id,image and types
+		// now iterate over each pokemon objects from array and extract name,id,image and types
 		let result = pokemonData.map((pokeData) => {
 			const pokemon = pokeData.data;
 			return {
@@ -41,8 +41,13 @@ const PokemonList = () => {
 
 	return (
 		<div className="pokemon-list-wrapper">
-			<h2>Pokemon list</h2>
-			{isloading ? "loading..........." : pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} />)}
+			<div className="pokemon-wrapper">
+				{isloading ? "loading..........." : pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} />)}
+			</div>
+			<div className="controls">
+				<button>Prev</button>
+				<button>Next</button>
+			</div>
 		</div>
 	);
 };
